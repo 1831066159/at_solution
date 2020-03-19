d79e733e1188adda9a729b6ca7b6a93a3b273794
zhaoxiang <zhaoxiang@huli.com>
2020-03-19 18:04:56 +0800

    dp

--- src/main/java/cn/com/cootoo/string/SMain.java
+++ src/main/java/cn/com/cootoo/string/SMain.java
@@ -21,6 +21,49 @@ public class SMain {
 
 
 //        checkdd("abcabcbb");
+//        System.out.println(huiwen());
+//
+        String s = "abcdcdabba";
+//        System.out.println(s.length());
+//        System.out.println(s.substring(0, s.length() - 2));
+
+        maxHuiWen(s);
+
+    }
+
+
+    private static void miniHuiWen(String s) {
+        // abcdcdabba
+        int n = s.length();
+        String res = new String();
+        boolean flag;
+        for (int r = 0; r < n; r++) {
+            for (int l = 0; l <= r; l++) {
+                flag = s.charAt(r) == s.charAt(l) && r - l < 3;
+                if (flag && r - l + 1 > res.length()) {
+                    res = s.substring(l, r + 1);
+                }
+            }
+        }
+        System.out.println(res);
+
+    }
+
+
+    private static void maxHuiWen(String s) {
+        // abcdcdabba
+        int n = s.length();
+        String res = new String();
+        boolean flag[]=new boolean[n];
+        for (int r = 0; r < n; r++) {
+            for (int l = 0; l <= r; l++) {
+                flag[l] = s.charAt(r) == s.charAt(l) &&  (r - l < 3 || flag[l+1]);
+                if (flag[l] && r - l + 1 > res.length()) {
+                    res = s.substring(l, r + 1);
+                }
+            }
+        }
+        System.out.println(res);
 
     }
 
@@ -43,10 +86,48 @@ public class SMain {
     }
 
 
-
-    public void huiwen() {
+    public static String huiwen() {
         // abcdcdabba
+        // p(i,j) && si=sj, p(i+1,j-1) && si+1=sj-1
+        String s = "abcdcdabba";
+        if (s.isEmpty()) {
+            return s;
+        }
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int left = 0;
+        int right = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][i] = true;
+            for (int j = i + 1; j < n; j++) {
+                System.out.println("i=" + i + ":" + s.charAt(i) + ",j=" + j + ":" + s.charAt(j) + "," + dp[i + 1][j - 1]);
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);//小于3是因为aba一定是回文
+                if (dp[i][j] && right - left < j - i) {
+                    left = i;
+                    right = j;
+                }
+            }
+        }
+        return s.substring(left, right + 1);
+    }
 
+
+    public static String hw() {
+        String s = "abcdcdabba";
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int l = 0, r = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][i] = true;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+            }
+        }
+
+
+        return "";
     }
 
 
